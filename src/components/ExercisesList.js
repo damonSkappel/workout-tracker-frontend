7a97b295082910397306d7299795fce278420b84
damonSkappel <dskappel@cord.edu>
2026-02-18 18:50:43 -0600

    Added the TemplateList page and started working on the ExerciseList page that comes after it

---
+++ src/components/ExercisesList.js
@@ -0,0 +1,37 @@
+import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import "./ExercisesList.css";
+
+const ExercisesList = () => {
+  const [exercises, setExercises] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect running!");
+
+    axios
+      .get(`http://localhost:3000/api/templates/1/exercises`)
+      .then((response) => {
+        console.log("API response:", response.data);
+        setExercises(response.data);
+      })
+      .catch((error) => {
+        console.log("API error:", error);
+      });
+  }, []);
+
+  return (
+    <div className="card-container">
+      <h2>Exercises</h2>
+
+      {exercises &&
+        exercises.map((exercise) => (
+          <div key={exercise.id} className="template-card">
+            <h3>{exercise.exercise_name}</h3>
+          </div>
+        ))}
+    </div>
+  );
+};
+export default ExercisesList;
--- src/components/TemplateList.js
+++ src/components/TemplateList.js
@@ -1,2 +1,38 @@
-//short change to test git
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
+import { useEffect } from "react";
+import "./TemplateList.css";
+
+const WorkoutTemplates = () => {
+  const [templates, setTemplates] = useState([]);
+
+  useEffect(() => {
+    console.log("useEffect running!");
+
+    axios
+      .get("http://localhost:3000/api/templates")
+      .then((response) => {
+        console.log("API response:", response.data);
+        setTemplates(response.data);
+      })
+      .catch((error) => {
+        console.log("API error:", error);
+      });
+  }, []);
+
+  return (
+    <div className="card-container">
+      <h2> Workout Templates</h2>
+
+      {templates &&
+        templates.map((template) => (
+          <div key={template.id} className="template-card">
+            <h3>{template.name}</h3>
+            <p>{"Last Session: " + template.created_at}</p>
+          </div>
+        ))}
+    </div>
+  );
+};
+export default WorkoutTemplates;
